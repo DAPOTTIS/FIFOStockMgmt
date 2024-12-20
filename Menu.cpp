9241f092fe70ab52794886b2ecd26dfb2aef97f2
ms <mohamedsaafan04@hotmail.com>
2024-12-21 00:43:18 +0200

    fixed el message el fel remove stock tany

--- Menu.cpp
+++ Menu.cpp
@@ -61,7 +61,8 @@ bool Menu::handleUserChoice() {
             break;
         case 3:
             removeStock();
-            cout << "Item first in stock has been removed.\n Please press any key to continue" << endl;
+            cout << "\nItem first in stock has been removed.\nPlease press any key to continue" << endl;
+            cin.ignore();
             getchar();
             break;
         case 4:
