9d507cc583a08e7420c630fe23957d724ae77fbb
Zeyad Khalaf <zeyadkhalaf2004@gmail.com>
2024-12-20 19:53:57 +0200

    Fy haga 8alat ana msh 3aref eh heya

--- Menu.cpp
+++ Menu.cpp
@@ -2,86 +2,90 @@
 #include "Menu.h"
 #include "FileHandler.h"
 #include "PasswordManager.h"
+
 using namespace std;
+
 void Menu::displayMenu() {
-    cout << "\tMAIN MENU" << endl << "__________________________" <<
-         endl << "ADD STOCK       [1]" << endl <<
-         "VIEW STOCK      [2]" << endl <<
-         "EDIT STOCK      [3]" << endl <<
-         "DELETE STOCK    [4]" << endl <<
-         "EDIT PASSWORD   [5]" << endl <<
-         "EXIT            [6]" << endl;
+    cout << "\tMAIN MENU" << endl
+         << "__________________________" << endl
+         << "ADD STOCK       [1]" << endl
+         << "VIEW STOCK      [2]" << endl
+         << "EDIT STOCK      [3]" << endl
+         << "DELETE STOCK    [4]" << endl
+         << "EDIT PASSWORD   [5]" << endl
+         << "EXIT            [6]" << endl;
 }
 
 int Menu::getUserChoice() {
     int choice;
-    cout << "Please enter an option : "<<endl;
-    cin>>choice;
+    cout << "Please enter an option : " << endl;
+    cin >> choice;
     while (choice < 1 || choice > 6) {
-        cout << "Invalid,Please enter another choice between 1 and 6:";
-        cin>>choice;
+        cout << "Invalid, Please enter another choice between 1 and 6: ";
+        cin >> choice;
     }
-return choice;
+    return choice;
 }
 
 void Menu::displayQueue() {
-    cout <<"Displaying the current stock in queue :"<<endl;
-    //n3mel implement hena ba2 men el queue
+    //from Queue file
 
-}
+    }
 
-void Menu::addStock(Node *input) {
-if (input == NULL) {
-    cout<<"Invalid,Please try again"<<endl;
-    return;
-}
+
+void Menu::addStock(Node* input) {
+    if (input == nullptr) {
+        cout << "Invalid, Please try again" << endl;
+        return;
+    }
     stockQueue.enqueue(input);
-    cout<<"Item added to stock successfully"<<endl;
+    cout << "Item added to stock successfully" << endl;
 
-FileHandler fileHandler;
+    FileHandler fileHandler;
     fileHandler.save(&stockQueue);
-    cout<<"Item has been saved to the file"<<endl;
+    cout << "Item has been saved to the file" << endl;
 }
 
 void Menu::removeStock() {
+    FileHandler fileHandler;
+    Queue stockQueue = fileHandler.loadFromFile();
+
     if (stockQueue.isEmpty()) {
-        cout<<"The stock is empty"<<endl;
+        cout << "The stock queue is empty" << endl;
         return;
     }
+
     Node* removedItem = stockQueue.dequeue();
     if (removedItem != nullptr) {
-        std::cout << "item removed successfully: "
-                  << removedItem->name << std::endl; // Adjust to match Node's fields
-        delete removedItem; // Free memory
+        cout << "Item removed successfully: " << removedItem->name << endl;
+        delete removedItem;
     } else {
-        std::cout << "Failed to remove item." << std::endl;
+        cout << "Failed to remove item." << endl;
     }
 
-    // Save the updated queue to the file
-    FileHandler fileHandler;
-    fileHandler.saveToFile(&stockQueue);
-    std::cout << "Item has been saved to the file." << std::endl;
+    fileHandler.save(&stockQueue);
+    cout << "stock has been saved to the file" << endl;
 }
 
-
-bool Menu:: authentication(){
-  PasswordManager passwordManger;
+bool Menu::authentication() {
+    PasswordManager passwordManager;
     string enterPass;
 
-    if (!passwordManger.checkPasswordAvailability()) {
-        cout<<"There is not a stored password,please set a new password."<<endl;
-        passwordManger.setPassword("");
+    if (!passwordManager.checkPasswordAvailability()) {
+        cout << "There is no stored password, please set a new password." << endl;
+        passwordManager.setPassword("");
         return true;
     }
-    cout<<"Please enter your password:"<<endl;
+
+    cout << "Please enter your password:" << endl;
     cin.ignore();
     getline(cin, enterPass);
-    if (passwordManger.getPassword(enterPass)) {
-        cout<<"Success!!"<<endl;
+
+    if (passwordManager.getPassword(enterPass)) {
+        cout << "Success!!" << endl;
         return true;
-    }
-    else {
-        cout<<"Failed,Please try again."<<endl;
+    } else {
+        cout << "Failed, Please try again." << endl;
         return false;
     }
 }
