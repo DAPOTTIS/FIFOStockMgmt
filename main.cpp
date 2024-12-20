036c34f2de49736dac9bd4f02736b0aa883fbf8f
ms <mohamedsaafan04@hotmail.com>
2024-12-21 00:26:18 +0200

    cleaned up main.cpp

--- main.cpp
+++ main.cpp
@@ -6,18 +6,13 @@
 int main() {
     Menu menu;
     system(CLEAR);
-    bool trueUser;
-check:
-   while(menu.authentication()){
-       trueUser =true;
-     menu.displayMenu();
-       while(!menu.handleUserChoice()) {
-           menu.displayMenu();
-       }
-   }
-while(!trueUser){
-        goto check;
-   }
+    while(!menu.authentication()) {
+        cout << "\n";
+    }
+    do {
+        menu.displayMenu();
+    }while(!menu.handleUserChoice());
+
 
     return 0;
 }
